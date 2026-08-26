87fcd41aa32730a1d03b4d10749d027c51ea4a21
anirudh-31 <betrabetanirudh@gmail.com>
2026-08-26 10:19:12 +0530

    adding origin and operation tracking

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -3,8 +3,10 @@
 """
 
 class Value:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, data, _children = (), _op = ''):
+        self.data  = data
+        self._prev = set(_children) # stores the set of 'values' that the current value originates from
+        self._op   = _op            # stores the operation that resulted in the current value
 
     def __repr__(self):
         return f"Value(data={self.data})"
@@ -12,10 +14,10 @@ class Value:
     def __add__(self, other) -> "Value":
         other = other if isinstance(other, Value) else Value(other)
         res = self.data + other.data
-        return Value(res)
+        return Value(res, (self, other), '+')
 
     def __mul__(self, other) -> "Value":
         other = other if isinstance(other, Value) else Value(other)
         res = self.data * other.data
-        return Value(res)
+        return Value(res, (self, other), '*')
 
--- scripts/test_value.py
+++ scripts/test_value.py
@@ -1,8 +1,13 @@
 from micrograd.engine import Value
 
-a = Value(4)
-b = Value(2)
+a = Value(2)
+b = Value(-3)
+c = Value(10)
+
+d = a * b + c
 print(f"a = {a}")
 print(f"b = {b}")
-print(f"a + b = {a + b}")
-print(f"a * b = {a * b}")
+print(f"c = {c}")
+print(f"d = a * b + c : {d}")
+print(f"d is derived from = {d._prev}")
+print(f"d resulted from the operation: {d._op}")
