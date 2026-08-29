b045733e4020b1af27f6d5428febc40c9231735b
anirudh-31 <betrabetanirudh@gmail.com>
2026-08-29 10:35:37 +0530

    visualize value chain

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -3,10 +3,11 @@
 """
 
 class Value:
-    def __init__(self, data, _children = (), _op = ''):
+    def __init__(self, data, _children = (), _op = '', label =""):
         self.data  = data
         self._prev = set(_children) # stores the set of 'values' that the current value originates from
         self._op   = _op            # stores the operation that resulted in the current value
+        self.label = label          # stores the label of the value
 
     def __repr__(self):
         return f"Value(data={self.data})"
@@ -19,5 +20,5 @@ class Value:
     def __mul__(self, other) -> "Value":
         other = other if isinstance(other, Value) else Value(other)
         res = self.data * other.data
-        return Value(res, (self, other), '*')
+        return Value(res, (self, other), '×')
 
--- scripts/test_value.py
+++ scripts/test_value.py
@@ -1,13 +1,21 @@
 from micrograd.engine import Value
+from micrograd.DrawDot import draw_dot
 
-a = Value(2)
-b = Value(-3)
-c = Value(10)
+a = Value(2, label="A")
+b = Value(-3, label="B")
+c = Value(10, label="C")
 
-d = a * b + c
+d = a * b
+d.label = "D"
+
+e = d + c
+e.label = "E"
+
 print(f"a = {a}")
 print(f"b = {b}")
 print(f"c = {c}")
-print(f"d = a * b + c : {d}")
-print(f"d is derived from = {d._prev}")
-print(f"d resulted from the operation: {d._op}")
+print(f"d = {d}")
+print(f"e = {e}")
+
+dot = draw_dot(e)
+dot.render("test_chain_operation", view=True)
